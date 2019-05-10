6ca324edfc73f070f248103f78ee52bf9bb2beaa
lilihongjava <lilihongbeast@163.com>
2019-05-10 17:23:48 +0800

    快速开发demo

--- quick_start/main.py
+++ quick_start/main.py
@@ -6,11 +6,27 @@
 @desc: 
 """
 import pandas as pd
+from fbprophet import Prophet
+from pandas.plotting import register_matplotlib_converters
 
 
 def main():
     df = pd.read_csv('./data/example_wp_log_peyton_manning.csv')
     print(df.head())
+    m = Prophet()
+    m.fit(df)
+
+    future = m.make_future_dataframe(periods=365)
+    print(future.tail())
+
+    forecast = m.predict(future)
+    print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+
+    register_matplotlib_converters()
+    fig1 = m.plot(forecast)
+    fig1.show()
+    fig2 = m.plot_components(forecast)
+    fig2.show()
 
 
 if __name__ == "__main__":
